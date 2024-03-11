0745eee3142762325435b312d149b9257b7d37ba
Eliz Erdim <elizerdim@gmail.com>
2024-03-11 20:32:28 +0300

    Add functionality for tracking color change

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ for (let i = 1; i <= 16 * 16; i++) {
   boardSquare.classList.add("board-square");
   drawingBoard.appendChild(boardSquare);
 }
+
+//track color change
+let colorPicker = document.querySelector("input[type=color]");
+let currentColor = colorPicker.value;
+
+colorPicker.addEventListener("change", e => {
+  currentColor = e.target.value;
+});
